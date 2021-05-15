91f3d8842896af4ed1e216b2d5a7d37ffe7ae2e4
rohit <rohithp61@gmail.com>
2021-05-15 22:24:56 +0530

    boiler plate for team page

--- Client/src/components/TeamPage/index.js
+++ Client/src/components/TeamPage/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Mainheader from "../MainHeader";
+import TeamBody from "./TeamBody";
 
-export default function TeamPage(){
+export default function LoginPage(){
     return(
         <div>
             <Mainheader />
+            <TeamBody />
         </div>
     )
 }
