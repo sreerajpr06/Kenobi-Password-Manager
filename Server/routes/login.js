ef010b2b7a4809f13e03136f2a43181cd5b248c8
Sidhant Unnithan <contact@sidhantunnithan.com>
2021-05-16 20:45:06 +0530

    Minor Fixes
    Fixed routes in the backend

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,13 +1,12 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+let User = require("../models/user.model");
 
-router.route('/').get((req, res) => {
-    const username = req.body.username;
-
-    User.find( { "username" : { $eq: username } })
-        .then( (user) => res.json(user))
-        .catch( err => res.status(400).json('Error: ' + err));
-
+// While logging in
+router.route("/").get((req, res) => {
+    const username = req.query.username;
+    User.findOne({ username: { $eq: username } })
+        .then((user) => res.json(user))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,20 +11,29 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+console.log(uri);
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
-connection.once( 'open' , () => {
-    console.log("MongoDB connection established");
-})
+connection
+    .once("open", () => {
+        console.log("MongoDB connection established");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login');
-const dashboardRouter = require('./routes/dashboard');
+const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+const dashboardRouter = require("./routes/dashboard");
 
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/dashboard', dashboardRouter);
+app.use("/users", usersRouter);
+app.use("/login", loginRouter);
+app.use("/dashboard", dashboardRouter);
 
 app.listen(port, () => {
     console.log("Port: " + port);
-})
+});
