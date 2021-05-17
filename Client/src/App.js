36e2480ef2e8456a4981dd8940993ee4039590b0
Sidhant Unnithan <contact@sidhantunnithan.com>
2021-05-17 18:14:35 +0530

    Minor Fixes
    Formatting Fixes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,29 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router,Route,Switch,Link,Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link,
+    Redirect,
+} from "react-router-dom";
 import Register from "./components/Register";
 import MainPage from "./components/MainPage";
 import Login from "./components/LoginPage";
 import Dashboard from "./components/Dashboard";
 import Team from "./components/TeamPage";
 
-
-class App extends Component{
+class App extends Component {
     render() {
-        <MainPage />
-        return <Router>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/team" component={Team} />
-        
-            
-        </Router>
-    
+        <MainPage />;
+        return (
+            <Router>
+                <Route exact path="/" component={MainPage} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/team" component={Team} />
+            </Router>
+        );
     }
 }
 
