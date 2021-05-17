91ef787e5f856ac0c8ee988ba823397d2ddff169
Sidhant Unnithan <contact@sidhantunnithan.com>
2021-05-18 00:08:46 +0530

    Added Register props

--- Client/src/components/Register/index.js
+++ Client/src/components/Register/index.js
@@ -3,11 +3,11 @@ import Mainheader from "../MainHeader";
 import RegisterBody from "./RegisterBody";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-export default function MainPage() {
+export default function Register(props) {
     return (
         <div>
             <Mainheader />
-            <RegisterBody />
+            <RegisterBody props={props} />
         </div>
     );
 }
